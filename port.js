afbc347a68cb7e0706e0119ed4f41af296ac1c10
Beijing-corn <96094900+Beijing-corn@users.noreply.github.com>
2023-02-05 20:20:55 -0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,10 @@
+function hidenseek(){
+    let oogabooga = document.getElementById("Menushow")
+    console.log(oogabooga)
+if(oogabooga.className == "lists"){
+oogabooga.className += "Menushow"
+}
+else{
+    oogabooga.className = "lists"
+}
+}
